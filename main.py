3cdb78f8297ee818be4e403b3176bdceeb409a11
Dawson Dittmar <dawson.dittmar@gmail.com>
2023-10-12 10:30:06 -0500

    started on scrolling

--- main.py
+++ main.py
@@ -2,16 +2,33 @@ from kivy.app import App
 from kivy.uix.screenmanager import Screen
 
 
+
 class PhotoGalleryApp(App):
     pass
 
 
 class Display(Screen):
+    images = ["download.jpg", "download2.jfif"]
+    index = 0
     def display_image(self):
-        return images[index]
+        return self.images[self.index]
+        self.ids.image.source = self.images[self.index]
 
-images = ["download.jpg"]
-index = 0
+    def advance(self):
+        self.index += 1
+        print(self.index)
+        if self.index > len(self.images):
+            self.index = 0
+        self.display_image()
+
+    def go_back(self):
+        self.index -= 1
+        print(self.index)
+        if self.index < 0:
+            self.index = 1
+        self.display_image()
+
+
 
 
 PhotoGalleryApp().run()
